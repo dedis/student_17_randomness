93b6843a81ed452b4c511de4224d1b18fa11aab8
Mathilde Raynal <mathilde.raynal@epfl.ch>
2017-12-27 15:13:12 +0100

    New organization

--- randshare_with_pvss/randshare.go
+++ randshare_with_pvss/randshare.go
@@ -1,17 +1,22 @@
-package randsharepvss
+package main
 
 import (
-	"time"
-
+	"fmt"
+	"github.com/dedis/student_17_randomness/randsharepvss"
 	"gopkg.in/dedis/onet.v1"
 	"gopkg.in/dedis/onet.v1/log"
+	"time"
 )
 
 func main() {
 
+	fmt.Print("How many nodes would you like to work with ? (between 0 and 100) : ")
+	var input int
+	fmt.Scanln(&input)
+	fmt.Print(input)
+
 	var name = "RandShare"
-	var nodes = 13
-	// 2/3 would prevent network splitting attacks
+	var nodes = input
 	var faulty = nodes / 3
 	var purpose = "RandShare test run"
 
@@ -22,30 +27,35 @@ func main() {
 	log.Lvlf1("randShare starting")
 	protocol, err := local.CreateProtocol(name, tree)
 	if err != nil {
-		t.Fatal("couldn't initialize", err)
+		log.LLvlf1("couldn't initialize %s", err)
+		return
 	}
 	rs := protocol.(*RandShare)
 	startingTime := time.Now().Unix()
 	err = rs.Setup(nodes, faulty, purpose, startingTime)
 	if err != nil {
-		t.Fatal("couldn't initialize", err)
+		log.LLvlf1("couldn't initialize %s", err)
+		return
 	}
 	err = rs.Start()
 	if err != nil {
-		t.Fatal(err)
+		log.LLvlf1("couldn't start %s", err)
+		return
 	}
 	select {
 	case <-rs.Done:
 		log.Lvlf1("RandShare done")
 		random, transcript, err := rs.Random()
 		if err != nil {
-			t.Fatal(err)
+			log.LLvlf1("Random failed %s", err)
+			return
 		}
 		if err = Verify(random, transcript); err != nil {
-			t.Fatal(err)
+			log.LLvlf1("couldn't verify %s", err)
+			return
 		}
 		log.Lvlf1("RandShare verified")
 	case <-time.After(time.Second * time.Duration(nodes) * 2):
-		t.Fatal("RandShare timeout")
+		log.LLvlf1("RandShare timeout")
 	}
 }
